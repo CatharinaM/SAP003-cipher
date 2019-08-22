872a3705e8ad4971f96cee84b75a367e9e5bec2d
Catharina <cathy.c@hotmail.com>
2019-08-22 16:42:37 -0300

    coloquei a função que cifra e que decifra

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 function Enviar1(){
-    let inputMensagem1 = document.getElementById("msgc").value;
-    let inputChave = document.getElementById("chaveoffset").value;
-    let resultEnviar1 = cipher.encode(inputChave,inputMensagem1);
 
-document.getElementById("result").innnerHTML= "Sua mensagem é: ${resultEnviar1} ";
+    let Mensagem1 = document.getElementById("msgc").value;
+    let Chave = Number(document.getElementById("chaveoffset").value);
+    let resultEnviar1 = window.cipher.encode(Mensagem1, Chave);
+
+
+   document.getElementById("resultEnviar1").innerHTML += resultEnviar1;
+    
+
 }
 
+function Enviar2(){
 
-// function Enviar2(){
-//     let inputMensagem2 = document.getElementById("msgd").value;
-//     let inputChave = document.getElementById("chaveoffset").value;
-//     return decode(inputChave,inputMensagem2);
+    let Mensagem2 = document.getElementById("msgd").value;
+    let Chave = Number(document.getElementById("chaveoffset").value);
+    let resultEnviar2 = window.cipher.decode(Mensagem2, Chave);
 
-// }
+
+   document.getElementById("resultEnviar2").innerHTML += resultEnviar2;
+}
+
 
