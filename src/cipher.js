d42acabc07e48a0435fa549b91bb12789be4d79a
Catharina <cathy.c@hotmail.com>
2020-01-30 18:03:38 -0300

    refatorei

--- src/cipher.js
+++ src/cipher.js
@@ -5,7 +5,7 @@ window.cipher = {
 
 function encode(messageEncode, key) {
   let printEncode="";
-  for (let i=0; i < messageEncode.length; i++) {
+  for (let i in messageEncode) {
     let changeEnc = messageEncode[i].charCodeAt();
     if (changeEnc >= 65 && changeEnc <= 90) {
       let cipherEcondeUppercase = ((changeEnc - 65 + key)%26) + 65;
@@ -14,8 +14,7 @@ function encode(messageEncode, key) {
       let cipherEncodelowercase = ((changeEnc - 97 + key)%26) + 97;
       printEncode += String.fromCharCode(cipherEncodelowercase );
     } else {
-      let resultCipherEncode = ((changeEnc - 32 + key)%33) + 32;
-      printEncode += String.fromCharCode(resultCipherEncode);
+      printEncode += messageEncode[i];
     }
   }
   return printEncode;
@@ -23,7 +22,7 @@ function encode(messageEncode, key) {
 
 function decode(messageDecode, key) {
   let printDecode="";
-  for (let i=0; i < messageDecode.length; i++) {
+  for (let i in messageDecode) {
     let changeDec = messageDecode[i].charCodeAt();
     if (changeDec >= 65 && changeDec <= 90) {
       let cipherDecodeUppercase = ((changeDec - 90 - key)%26) + 90;
@@ -32,8 +31,7 @@ function decode(messageDecode, key) {
       let cipherDecodelowercase = ((changeDec - 122 - key)%26) + 122;
       printDecode += String.fromCharCode(cipherDecodelowercase);
     } else {
-      let resultCipherDecode = ((changeDec - 64 - key)%33) + 64;
-      printDec += String.fromCharCode(resultCipherDecode);
+      printDecode += messageDecode[i];
     }
   } 
   return printDecode;
